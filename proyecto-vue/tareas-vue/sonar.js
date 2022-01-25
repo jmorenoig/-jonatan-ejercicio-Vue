048a49d5e3bf59459ccbe79966a196b9070c7033
jmorenoig <jmoreno@intergrupo.com>
2022-01-25 10:48:51 -0500

    Sonar publish

--- proyecto-vue/tareas-vue/sonar.js
+++ proyecto-vue/tareas-vue/sonar.js
@@ -4,15 +4,14 @@ scanner(
         serverUrl : 'http://sonarjason22.eastus.cloudapp.azure.com:9000/',
         token : "180f50452f04092ffa56f722c918df4434e9973d",
         options: {
-                'sonar.projectKey': 'vue-jonatan',
-                'sonar.projectName': 'vue-jonatan',
+                'sonar.projectKey': 'Juan-vue-Jenkins',
+                'sonar.projectName': 'Juan-vue-Jenkins',
                 'sonar.projectVersion': '1.0',
-                //'sonar.sources': '.',
-                //'sonar.tests': 'tests/unit',
-                //'sonar.inclusions': '**', // Entry point of your code
-                //'sonar.test.inclusions': 'tests/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
-                'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info'
-                //'sonar.testExecutionReportPaths': 'coverage/test-reporter.xml'
+                'sonar.sources': 'src',
+                'sonar.tests': 'tests/unit',
+                'sonar.test.inclusions': 'tests/unit/**/*.spec.js,tests/unit/**/*.spec.jsx', // Entry point of your code
+                'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
+                'sonar.testExecutionReportPaths': 'test-reporter.xml'
                 }
         },
         () => process.exit());
